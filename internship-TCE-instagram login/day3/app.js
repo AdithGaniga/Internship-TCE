4708650e32f8341c33eb81fcc84ecb0c3be1ff82
Adith <114330730+AdithGaniga@users.noreply.github.com>
2023-10-14 20:03:49 +0530

    Add files via upload

---
+++ internship-TCE-instagram login/day3/app.js
@@ -0,0 +1,27 @@
+const v="Why are you gay?";
+// prompt("Enter your name and gender","");
+console.log("This is console");
+// console.log("ashwin bvc");
+document.write("<h1>  <h1> ")
+// prompt(v);
+// let num2 = 9
+// num2 =11
+// console.log(num2);
+
+let a = 5
+b=a+3
+// console.log(a == b);
+// const btnn = document.querySelector(".btn1");
+// btnn.addEventListener("mouseover",()=>{greet()})
+// function greet(){
+//     alert("Hello")
+// }
+
+// for (let num=10; num >=1,num--){
+//     console
+// }
+const btnn1 = document.getElementsByClassName("btn1");
+btnn1.addEventListener("mouseover",()=>{greet()})
+function greet(){
+    alert("Hello")
+}
