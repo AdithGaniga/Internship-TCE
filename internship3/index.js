456f9abf0d6c9f71548b5874087df4b059aa7318
Adith <114330730+AdithGaniga@users.noreply.github.com>
2023-10-14 20:16:25 +0530

    Add files via upload

---
+++ internship3/index.js
@@ -0,0 +1,26 @@
+function calculate() {
+    const operator = document.getElementById("operator").value;
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    let result;
+
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            result = num1 / num2;
+            break;
+        default:
+            result = "Invalid operator";
+            break;
+    }
+
+    document.getElementById("result").innerHTML = `Result: ${result}`;
+}
